e04afe5c25b78bcce34f47b6ab5e402f37ebae0e
kunalshit <kunalshit21@gmail.com>
2025-05-11 09:06:07 -0700

    adding

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import {BrowserRouter , Routes ,Route} from "react-router-dom";
 import Admin_dashbord from './Component_Pages/Admin/Admin_dashbord';
 import Registration_Page from './Component_Pages/Authentications/Registration_Page';
+import Login_Page from './Component_Pages/Authentications/Login_Page';
 const App = () => {
   return <>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Admin_dashbord/>}></Route>
         <Route path='/Reg' element={<Registration_Page/>}></Route>
+        <Route path='/Log' element={<Login_Page/>}></Route>
       </Routes>
     </BrowserRouter>
   </>
--- Frontend/src/Component_Pages/Authentications/Login_Page.jsx
+++ Frontend/src/Component_Pages/Authentications/Login_Page.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Logoweb from '/src/Images/Logoweb.png';
 import { Link,NavLink } from 'react-router-dom';
-const Registration_Page = () => {
+
+const Login_Page = () => {
     return <>
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 mt-30 ">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -11,30 +12,14 @@ const Registration_Page = () => {
                     className="mx-auto h-10 w-auto"
                 />
                 <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-                    Sign up to your account
+                    Sign in to your account
                 </h2>
             </div>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <form action="#" method="POST" className="space-y-6">
 
-                    <div>
-                        <label htmlFor="roll" className="block text-sm/6 font-medium text-gray-900">
-                            Name
-                        </label>
-                        <div className="mt-2">
-                            <input
-                                id="name"
-                                name="name"
-                                type="text"
-                                required
-                                autoComplete="text"
-                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                            />
-                        </div>
-                    </div>
-
-
+                
                     <div>
                         <label htmlFor="roll" className="block text-sm/6 font-medium text-gray-900">
                             Roll number
@@ -56,7 +41,11 @@ const Registration_Page = () => {
                             <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
                                 Password
                             </label>
-                            
+                            <div className="text-sm">
+                                <a href="#" className="font-semibold text-indigo-900 hover:text-indigo-500">
+                                    Forgot password?
+                                </a>
+                            </div>
                         </div>
                         <div className="mt-2">
                             <input
@@ -75,14 +64,14 @@ const Registration_Page = () => {
                             type="submit"
                             className="flex w-full justify-center rounded-md bg-indigo-900 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
-                            Sign up
+                            Sign in
                         </button>
                     </div>
                 </form>
 
                 <p className="mt-10 text-center text-sm/6 text-gray-500">
-                    Already have an account?{' '}
-                    <Link to="/Log"><a className="font-semibold text-indigo-900 hover:text-indigo-500">
+                    do not have an account?{' '}
+                    <Link to="/Reg"><a className="font-semibold text-indigo-900 hover:text-indigo-500">
                         click
                     </a></Link>
                 </p>
@@ -91,4 +80,4 @@ const Registration_Page = () => {
     </>
 }
 
-export default Registration_Page
+export default Login_Page
